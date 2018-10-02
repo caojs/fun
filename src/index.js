99be529b96f33915b5c55ff2aecfb839f835f6ab
dai-cao <dai.cao@vn.devinition.com>
2018-10-02 16:59:46 +0700

    add support ie11

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import 'react-app-polyfill/ie11';
+import 'cross-fetch/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import 'cross-fetch/polyfill';
 import 'bootstrap/dist/css/bootstrap.css';
 import './common.css';
 import './index.css';
