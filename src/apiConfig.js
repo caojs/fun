ff290e760b6bdd0826847c6c5234a12120f624cd
dai-cao <dai.cao@vn.devinition.com>
2018-10-09 10:35:08 +0700

    update port

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
-export const rootApi = window.location.protocol + "//" + window.location.hostname + ":2000/";
+export const rootApi = window.location.protocol + "//" + window.location.hostname + ":4000/";
 export const filterApi = "filtering";
