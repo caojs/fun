abf435ea8ad897a7717479154d1e39f8ef5b2617
caojs <caoo.dai@gmail.com>
2018-12-09 14:54:53 +0700

    update

--- src/containers/App/AuthenticationButtons/index.js
+++ src/containers/App/AuthenticationButtons/index.js
@@ -9,7 +9,7 @@ export default function AuthenticationButtons() {
     return (
         <div className={cn(styles.main, "d-inline-flex align-items-center")}>
             <NavLink className="ab-link" to="/login">Login</NavLink>
-            <span class="ab-seperator font-4"><FaCircle/></span>
+            <span className="ab-seperator font-4"><FaCircle/></span>
             <NavLink className="ab-link" to="/register">Register</NavLink>
         </div>
     )
